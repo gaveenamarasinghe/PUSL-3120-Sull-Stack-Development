4ca436a0f1c6bd2c6fda45df31114501acfc8dc7
Gaveen Amarasinghe <gaveenamarasinghe2004@gmail.com>
2024-12-23 20:04:41 +0530

    Do Some Changers of the Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SeatSelectorComponent } from "./seat-selector/seat-selector.component";
+import { SeatSelectorComponent } from './seat-selector/seat-selector.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SeatSelectorComponent],
+  imports: [SeatSelectorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'BOOKME-Web-Application';
+  title = 'BOOKME';
 }
- 

d2eacde2ff986741b4aca657f951ae881c415c97
kavindyabasnayake <135852012+kavindyabasnayake@users.noreply.github.com>
2024-12-26 22:42:02 +0530

    Dashboard_Bus_Admin
    Committing for Dashboard,  Bus registration and Admin panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BusRegistrationComponent } from './bus-registration/bus-registration.component';
+import { AdminComponent } from './admin/admin.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DashboardComponent, BusRegistrationComponent, AdminComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/bus-registration/bus-registration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bus-registration',
+  imports: [],
+  templateUrl: './bus-registration.component.html',
+  styleUrl: './bus-registration.component.css'
+})
+export class BusRegistrationComponent {
+
+}

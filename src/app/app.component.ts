4d1be56791f6dbfa905b5e6efc323f70b3380b9b
Nesandi Premathilaka <139942387+NesandiLaknavi@users.noreply.github.com>
2024-12-29 23:42:29 +0530

    create services page and add images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ServicesComponent } from "./services/services.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ServicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
